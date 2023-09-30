3917e69194244686631c46b10eab82baa66402bc
Aleksandr Levochkin <aleksandrlevochkin@Aleksandrs-MacBook-Air.local>
2023-10-01 01:15:58 +0200

    Create indexes for case insensitive search

--- db/mongo.ts
+++ db/mongo.ts
@@ -12,10 +12,27 @@ export async function connectToMongoDb() {
   try {
     await client.connect();
     console.log("Successfully connected to Mongo!");
+    await createIndexes();
   } catch (error) {
     console.error("Error occurred while connecting to Mongo!", error);
     await client.close();
   }
+}
+
+async function createIndexes(): Promise<void> {
+  const collectionName = "releases";
+  const db = client.db("rymdata");
+
+  const indexes = { artist: 1, album: 1, genres: 1, year: 1 };
+
+  const options = {
+    collation: {
+      locale: "en",
+      strength: 2,
+    },
+  };
+
+  await db.collection(collectionName).createIndex(indexes, options);
 }
 
 export function getClient() {
